622ef85c45deab36d6a7b33542ddf16f81b9b305
Ahmadreza322 <ahmadrezamirzaki2@gmail.com>
2026-02-06 13:25:09 +0330

    created first app
    created first app named website with three views for contact , about and index

---
+++ website/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+
+def index_view(request):
+    return HttpResponse ('Home page')
+
+def about_view(request):
+    return HttpResponse ('About page')
+
+def contact_view(request):
+    return HttpResponse ('Contact page')
+
+
